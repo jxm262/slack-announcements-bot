91d7c22f80c1b26583609c77e5acc95c1ecf8582
Justin <jxm262@gmail.com>
2017-02-12 13:24:48 -0500

    retrieving and printing each matching date, description

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import cheerio from 'cheerio'
 import request from 'superagent'
+import _ from 'lodash'
+
+
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 
 export default function app(cb) {
@@ -10,11 +14,26 @@ export default function app(cb) {
 
             const blogPostings = $('.blog-index \> li')
 
-            console.log(blogPostings.length);
-            blogPostings.foreach((elem, idx) => {
-                console.log('el ', elem);
-                //console.log("time ", elem.find('time'))
-                //return elem
+            console.log(typeof blogPostings);
+            blogPostings.each((idx, elem) => {
+
+                const time = $(elem).find('time').text().trim()
+                const m = time.split(" ")
+                const [day, monthShortName] = m
+
+                const date = new Date()
+                const currentMonth = monthNames[date.getUTCMonth()+1]
+
+                const utcDate = date.getUTCDate()
+                const currentDay = (utcDate < 10) ? "0" + utcDate : utcDate
+
+                const isCurrentMonth = currentMonth.includes(monthShortName)
+                const isCurrentDay = day == currentDay
+
+
+                console.log("date " + time);
+                console.log('title ', $(elem).find("a").text() + "\n\n");
+
             })
 
             cb()
